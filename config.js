a4fb5dcd1893f2a2835594593305c16cb804427e
Nino <ninofocus@gmail.com>
2015-10-16 12:59:44 +0800

    移除数据库支持

--- config.js
+++
@@ -1,9 +0,0 @@
-export default {
-    mysql: {
-        host: '127.0.0.2',
-        user: 'root',
-        password: '',
-        database: 'dict',
-        connectTimeout: '100'
-    }
-}
--- index.js
+++ index.js
@@ -1,41 +1,13 @@
 #!/usr/bin/env babel-node
 
 import Dict from './lib/dict'
-import co from 'co'
-import models, {connection} from './lib/models'
 import print from './lib/print'
 
 let q = process.argv.slice(2).join(' ')
 let dict = new Dict(q)
 
-co(function* (){
-    connection.connect()
-
-    // 查询数据库
-    var data = yield models.words.getByName(q);
-
-    if(data && data.length) {
-        data = data[0];
-        print(data.translate);
-    } else {
-        // 查询有道词典
-        data = yield dict.query();
-        print(data);
-        var errorCode = JSON.parse(data).errorCode;
-
-        if(errorCode == 0) {
-            // 保存到数据库
-            yield models.words.create(q, data);
-        }
-    }
-
-    connection.end();
-}).catch(function(err) {
-    if(err.code == 'ETIMEDOUT') {
-        dict.query().then(print);
-    } else {
-        console.error(err)
-    }
-
-    connection.end();
+dict.query().then((data) => {
+    print(data)
+}).catch((msg) => {
+    console.error(msg)
 })
